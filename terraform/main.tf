d78efcee2a07eca202b49e46db289dc84ff364ad
Lukasz <vvooki99@gmail.com>
2025-06-24 17:55:33 +0200

    initialize project structure

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "30ae2620-785b-4b34-8f06-34fdad77ee82"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "gh-terraform-aks"
+  location = "westeurope"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "gh-terraform-aks-cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "ghaks"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
